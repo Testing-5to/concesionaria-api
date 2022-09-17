92322842303566eff4d0057d160b9af138b87af3
LeooZeballos <zeballosleonel3@gmail.com>
2022-09-17 20:34:05 -0300

    fixed LocalidadService

--- src/main/java/com/autos/concesionaria/service/LocalidadService.java
+++ src/main/java/com/autos/concesionaria/service/LocalidadService.java
@@ -17,29 +17,55 @@ public class LocalidadService {
     // Repository injected by constructor
     private LocalidadRepository localidadRepository;
 
-    // Get all the localidades
-    public List<Localidad> buscarLocalidades() {
-        return localidadRepository.findAll();
-    }
-
-    // Get a localidad by id
-    public Localidad buscarLocalidadPorId(Long id) {
-        return localidadRepository.findById(id).orElseThrow();
-    }
-
-    // Create a localidad
+    /**
+     * Create a new localidad
+     *
+     * @param localidad
+     * @return Localidad created
+     */
     public Localidad crearLocalidad(Localidad localidad) {
         return localidadRepository.save(localidad);
     }
 
-    // Update a localidad
-    public Localidad actualizarLocalidadPorId(Long id, Localidad localidad) {
-        Localidad localidadActual = localidadRepository.findById(id).orElseThrow();
-        localidadActual.setNombre(localidad.getNombre());
-        return localidadRepository.save(localidadActual);
+    /**
+     * Get all the localidades
+     *
+     * @return List<Localidad> List of localidades
+     */
+    public List<Localidad> buscarLocalidades() {
+        return localidadRepository.findAll();
     }
 
-    // Delete a localidad
+    /**
+     * Get a localidad by id
+     *
+     * @param id
+     * @return Localidad found or null
+     */
+    public Localidad buscarLocalidadPorId(Long id) {
+        return localidadRepository.findById(id).get();
+    }
+
+    /**
+     * Update a localidad
+     *
+     * @param id        Localidad id
+     * @param localidad Localidad data to update
+     * @return Localidad updated
+     */
+    public Localidad actualizarLocalidadPorId(Long id, Localidad localidad) {
+        Localidad localidadActual = localidadRepository.findById(id).get();
+        localidadActual.setNombre(localidad.getNombre());
+        localidadActual.setCodigoPostal(localidad.getCodigoPostal());
+        localidadActual.setProvincia(localidad.getProvincia());
+        return localidadRepository.saveAndFlush(localidadActual);
+    }
+
+    /**
+     * Delete a localidad
+     *
+     * @param id Localidad id
+     */
     public void eliminarLocalidadPorId(Long id) {
         localidadRepository.deleteById(id);
     }
