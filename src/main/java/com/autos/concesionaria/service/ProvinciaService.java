0e7bfdcf371f422a78c4f49dd686a62877de691c
LeooZeballos <zeballosleonel3@gmail.com>
2022-09-17 19:43:18 -0300

    added Provincia Service

---
+++ src/main/java/com/autos/concesionaria/service/ProvinciaService.java
@@ -0,0 +1,69 @@
+package com.autos.concesionaria.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.autos.concesionaria.entity.Provincia;
+import com.autos.concesionaria.repository.ProvinciaRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProvinciaService {
+
+    @Autowired
+    // Repository injected by constructor
+    private ProvinciaRepository provinciaRepository;
+
+    /**
+     * Create a provincia
+     *
+     * @param pais
+     * @return Pais created
+     */
+    public Provincia crearProvincia(Provincia provincia) {
+        return provinciaRepository.save(provincia);
+    }
+
+    /**
+     * Get all the provincias
+     *
+     * @return List<Provincia> List of provincias
+     */
+    public List<Provincia> buscarProvincias() {
+        return provinciaRepository.findAll();
+    }
+
+    /**
+     * Get a provincia by id
+     *
+     * @param id
+     * @return Provincia found or null
+     */
+    public Provincia buscarProvinciaPorId(Long id) {
+        return provinciaRepository.findById(id).get();
+    }
+
+    /**
+     * Update a provincia
+     *
+     * @param id   Provincia id
+     * @param pais Provincia data to update
+     * @return Provincia updated
+     */
+    public Provincia actualizarProvinciaPorId(Long id, Provincia provincia) {
+        Provincia provinciaActual = provinciaRepository.findById(id).get();
+        provinciaActual.setNombre(provincia.getNombre());
+        return provinciaRepository.saveAndFlush(provinciaActual);
+    }
+
+    /**
+     * Delete a provincia
+     *
+     * @param id Provincia id
+     */
+    public void eliminarProvinciaPorId(Long id) {
+        provinciaRepository.deleteById(id);
+    }
+
+}
