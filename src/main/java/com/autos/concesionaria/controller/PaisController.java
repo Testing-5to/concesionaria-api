6f2e9bba5e1adfa32beabc92c0a5b621ff4ca405
LeooZeballos <48022327+LeooZeballos@users.noreply.github.com>
2022-09-17 17:03:43 +0000

    created PaisController

---
+++ src/main/java/com/autos/concesionaria/controller/PaisController.java
@@ -0,0 +1,57 @@
+package com.autos.concesionaria.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autos.concesionaria.entity.Pais;
+import com.autos.concesionaria.service.PaisService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/pais")
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@RequiredArgsConstructor
+public class PaisController {
+
+    private PaisService paisService;
+
+    // GET
+    @GetMapping
+    public ResponseEntity<?> getPaises() {
+        return new ResponseEntity<>(paisService.buscarPaises(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPaisPorId(@PathVariable Long id) {
+        return new ResponseEntity<>(paisService.buscarPaisPorId(id), HttpStatus.OK);
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity<Pais> guardarPais(@RequestBody Pais pais) {
+        return new ResponseEntity<>(paisService.crearPais(pais), HttpStatus.CREATED);
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<Pais> actualizarPais(@PathVariable Long id, @RequestBody Pais pais) {
+        return new ResponseEntity<>(paisService.actualizarPaisPorId(id, pais), HttpStatus.OK);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> borrarPais(@PathVariable Long id) {
+        return new ResponseEntity<>(paisService.borrarPaisPorId(id), HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/autos/concesionaria/service/PaisService.java
+++ src/main/java/com/autos/concesionaria/service/PaisService.java
@@ -1,5 +1,7 @@
 package com.autos.concesionaria.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.autos.concesionaria.entity.Pais;
@@ -13,12 +15,16 @@ public class PaisService {
 
     private PaisRepository paisRepository;
 
-    public Pais crearPais(Pais pais) {
-        return paisRepository.save(pais);
+    public List<Pais> buscarPaises() {
+        return paisRepository.findAll();
     }
 
     public Pais buscarPaisPorId(Long id) {
         return paisRepository.findById(id).get();
+    }
+
+    public Pais crearPais(Pais pais) {
+        return paisRepository.save(pais);
     }
 
     public Pais actualizarPaisPorId(Long id, Pais pais) {
