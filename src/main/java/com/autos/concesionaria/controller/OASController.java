50f71c2b3624fd0d25d176c91adc42966ff38141
LeooZeballos <zeballosleonel3@gmail.com>
2022-09-17 20:05:25 -0300

    swagger 3 en root

---
+++ src/main/java/com/autos/concesionaria/controller/OASController.java
@@ -0,0 +1,25 @@
+package com.autos.concesionaria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Root context redirection to OpenAPI API Specification (OAS) documentation.
+ *
+ * @author John Yeary <jyeary@bluelotussoftware.com>
+ * @version 1.0.0
+ */
+@Controller
+public class OASController {
+
+    /**
+     * Sets the index page mapping to point to the Swagger UI.
+     *
+     * @return A redirect to the Swagger UI.
+     */
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:v3/api-docs";
+    }
+
+}
