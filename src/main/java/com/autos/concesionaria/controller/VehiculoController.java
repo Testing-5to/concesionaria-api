4911ce90114a9bd6e047560944b52f16f3d496ea
LeooZeballos <zeballosleonel3@gmail.com>
2022-10-23 18:30:38 -0300

    vehiculo controller logs

--- src/main/java/com/autos/concesionaria/controller/VehiculoController.java
+++ src/main/java/com/autos/concesionaria/controller/VehiculoController.java
@@ -5,6 +5,8 @@ import com.autos.concesionaria.service.VehiculoService;
 
 import com.autos.concesionaria.service.VentaService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class VehiculoController {
 
+    // Logger
+    private static final Logger logger = LogManager.getLogger(VehiculoController.class);
+
     // Service injected by constructor
     @Autowired
     private final VehiculoService vehiculoService;
@@ -29,8 +34,10 @@ public class VehiculoController {
     @GetMapping
     public ResponseEntity<List<Vehiculo>> getVehiculos(@RequestParam(required = false) String modelo) {
         if (modelo == null) {
+            logger.info("Getting all the vehiculos");
             return new ResponseEntity<>(vehiculoService.getVehiculos(), HttpStatus.OK);
         } else {
+            logger.info("Getting all the vehiculos by modelo");
             return new ResponseEntity<>(vehiculoService.getVehiculosByModelo(modelo), HttpStatus.OK);
         }
     }
@@ -39,6 +46,7 @@ public class VehiculoController {
     // Get mapping to get a vehiculo by id
     @GetMapping("/{id}")
     public ResponseEntity<Vehiculo> getVehiculoPorId(@PathVariable Long id) {
+        logger.info("Getting the vehiculo with id: " + id);
         return new ResponseEntity<>(vehiculoService.getVehiculo(id), HttpStatus.OK);
     }
 
@@ -46,6 +54,7 @@ public class VehiculoController {
     // Post mapping to create a vehiculo
     @PostMapping
     public ResponseEntity<Vehiculo> guardarVehiculo(@RequestBody Vehiculo vehiculo) {
+        logger.info("Creating a new vehiculo");
         return new ResponseEntity<>(vehiculoService.crearVehiculo(vehiculo), HttpStatus.CREATED);
     }
 
@@ -53,6 +62,7 @@ public class VehiculoController {
     // Put mapping to update a vehiculo
     @PutMapping("/{id}")
     public ResponseEntity<Vehiculo> actualizarVehiculo(@PathVariable Long id, @RequestBody Vehiculo vehiculo) {
+        logger.info("Updating the vehiculo with id: " + id);
         return new ResponseEntity<>(vehiculoService.actualizarVehiculo(id, vehiculo), HttpStatus.OK);
     }
 
@@ -64,9 +74,11 @@ public class VehiculoController {
         if (ventaService.getVentasByVehiculo(id).isEmpty()) {
             // Si no existen ventas asociadas al vehiculo, lo borramos
             vehiculoService.borrarVehiculo(id);
+            logger.info("Deleting the vehiculo with id: " + id);
             return new ResponseEntity<>("Vehiculo borrado", HttpStatus.OK);
         } else {
             // Si existen ventas asociadas al vehiculo, no lo borramos
+            logger.info("The vehiculo with id: " + id + " can't be deleted because it has sales associated");
             return new ResponseEntity<>("No se puede borrar el vehiculo porque tiene ventas asociadas", HttpStatus.BAD_REQUEST);
         }
     }
