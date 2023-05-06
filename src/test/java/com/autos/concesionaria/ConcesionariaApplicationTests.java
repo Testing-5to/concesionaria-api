edba6ba91a3b9d221e0c99d62f5109955acd2e2c
LeooZeballos <zeballosleonel3@gmail.com>
2023-05-06 16:51:54 -0300

    test de pruba

--- src/test/java/com/autos/concesionaria/ConcesionariaApplicationTests.java
+++ src/test/java/com/autos/concesionaria/ConcesionariaApplicationTests.java
@@ -3,11 +3,21 @@ package com.autos.concesionaria;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ConcesionariaApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test() {
+        int a = 1;
+        int b = 2;
+
+        assertEquals(3, a + b);
     }
 
 }
