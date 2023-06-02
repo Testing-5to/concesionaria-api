1458b827eba5cb3f5fb716c3ce546e00fd029d12
LeooZeballos <zeballosleonel3@gmail.com>
2023-06-02 00:10:17 -0300

    refactoring de direccion controller

--- src/main/java/com/autos/concesionaria/controller/DireccionController.java
+++ src/main/java/com/autos/concesionaria/controller/DireccionController.java
@@ -24,11 +24,8 @@ public class DireccionController {
     private static final Logger logger = LoggerFactory.getLogger(DireccionController.class);
 
     // Inyección de dependencias de servicio
-    @Autowired
     private final DireccionService direccionService;
-    @Autowired
     private final EmpleadoService empleadoService;
-    @Autowired
     private final ClienteService clienteService;
 
     // GET
@@ -36,9 +33,9 @@ public class DireccionController {
     @GetMapping
     public ResponseEntity<List<Direccion>> getDirecciones(@RequestParam(required = false) String localidad) {
         if (localidad == null) {
-            return new ResponseEntity<List<Direccion>>(direccionService.buscarDirecciones(), HttpStatus.OK);
+            return ResponseEntity.ok(direccionService.buscarDirecciones());
         } else {
-            return new ResponseEntity<List<Direccion>>(direccionService.buscarDireccionesPorLocalidad(localidad), HttpStatus.OK);
+            return ResponseEntity.ok(direccionService.buscarDireccionesPorLocalidad(localidad));
         }
     }
 
@@ -46,7 +43,7 @@ public class DireccionController {
     // Obtener una dirección por ID
     @GetMapping("/{id}")
     public ResponseEntity<Direccion> getDireccionPorId(@PathVariable Long id) {
-        return new ResponseEntity<Direccion>(direccionService.buscarDireccionPorId(id), HttpStatus.OK);
+        return ResponseEntity.ok(direccionService.buscarDireccionPorId(id));
     }
 
     // POST
@@ -54,7 +51,7 @@ public class DireccionController {
     @PostMapping
     public ResponseEntity<Direccion> guardarDireccion(@RequestBody Direccion direccion) {
         logger.info("Guardando la direccion: " + direccion);
-        return new ResponseEntity<Direccion>(direccionService.crearDireccion(direccion), HttpStatus.CREATED);
+        return new ResponseEntity<>(direccionService.crearDireccion(direccion), HttpStatus.CREATED);
     }
 
     // PUT
@@ -62,7 +59,7 @@ public class DireccionController {
     @PutMapping("/{id}")
     public ResponseEntity<Direccion> actualizarDireccion(@PathVariable Long id, @RequestBody Direccion direccion) {
         logger.info("Actualizando la direccion con id: " + id);
-        return new ResponseEntity<Direccion>(direccionService.actualizarDireccionPorId(id, direccion), HttpStatus.OK);
+        return ResponseEntity.ok(direccionService.actualizarDireccionPorId(id, direccion));
     }
 
     // DELETE
@@ -72,11 +69,11 @@ public class DireccionController {
         // Verifico si la direccion esta asociada a un empleado o cliente
         if (empleadoService.contarEmpleadosPorDireccion(id) > 0 || clienteService.contarClientesPorDireccion(id) > 0) {
             logger.info("No se puede eliminar la direccion con id: " + id + " porque esta asociada a un empleado o cliente");
-            return new ResponseEntity<String>("No se puede eliminar la direccion con id: " + id + " porque esta asociada a un empleado o cliente", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("No se puede eliminar la direccion con id: " + id + " porque esta asociada a un empleado o cliente", HttpStatus.BAD_REQUEST);
         } else {
             logger.info("Eliminando la direccion con id: " + id);
             direccionService.eliminarDireccionPorId(id);
-            return new ResponseEntity<String>("Direccion eliminada", HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>("Direccion eliminada", HttpStatus.NO_CONTENT);
         }
     }
 
