5cdc1c191ff29f5767084ad9a06bca7d6a7564c7
LeooZeballos <zeballosleonel3@gmail.com>
2023-06-02 00:06:24 -0300

    100% de coverage de rol controller en tests

--- src/test/java/com/autos/concesionaria/controller/RolControllerTest.java
+++ src/test/java/com/autos/concesionaria/controller/RolControllerTest.java
@@ -1,74 +1,125 @@
 package com.autos.concesionaria.controller;
 
 import com.autos.concesionaria.entity.Rol;
+import com.autos.concesionaria.service.EmpleadoService;
 import com.autos.concesionaria.service.RolService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-class RolControllerTest {
-
-    @Autowired
-    private ObjectMapper mapper;
+@WebMvcTest(RolController.class)
+public class RolControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private RolService rolService;
 
+    @MockBean
+    private EmpleadoService empleadoService;
 
     @Test
-    public void getRolesSucces() throws Exception {
+    public void testGetRoles() throws Exception {
+        // Mockear el servicio para devolver una lista de países
+        List<Rol> roles = new ArrayList<>();
+        roles.add(new Rol(1L, "Administrador"));
+        roles.add(new Rol(2L, "Usuario"));
+        when(rolService.buscarRoles()).thenReturn(roles);
 
-        //create mocked entity
-        List<Rol> testResponse = new ArrayList<>();
-        testResponse.add(new Rol(1L, "vendedor"));
-
-
-        //mock the response of the method in the service
-        when(rolService.buscarRoles()).thenReturn(testResponse);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/api/v1/rol")
+        // Realizar la solicitud GET y verificar los resultados esperados
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/rol")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(roles)));
     }
 
     @Test
-    public void guardarRol() throws Exception {
-        //Create a mocked entity
-        Rol rolResponse = new Rol(1L, "vendedor");
+    public void testGetRol() throws Exception {
+        // Mockear el servicio para devolver un rol
+        Rol rol = new Rol(1L, "Administrador");
+        when(rolService.buscarRolPorId(1L)).thenReturn(rol);
 
-        //mock the response of the method in the service
-        when(rolService.crearRol(rolResponse)).thenReturn(rolResponse);
-
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/api/v1/rol").content(toJson(rolResponse))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+        // Realizar la solicitud GET y verificar los resultados esperados
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/rol/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.nombre").value("Administrador"));
     }
 
-    private String toJson(Rol rolResponse) throws JsonProcessingException {
-        return mapper.writeValueAsString(rolResponse);
+    @Test
+    public void testCrearRol() throws Exception {
+        // Mockear el servicio para devolver un rol
+        Rol rol = new Rol(1L, "Administrador");
+        when(rolService.crearRol(any(Rol.class))).thenReturn(rol);
+
+        // Realizar la solicitud POST y verificar los resultados esperados
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/rol")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(rol)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.nombre").value("Administrador"));
+    }
+
+    @Test
+    public void testActualizarRol() throws Exception {
+        // Mockear el servicio para devolver un rol
+        Rol rol = new Rol(1L, "Administrador");
+        when(rolService.actualizarRolPorId(eq(1L), any(Rol.class))).thenReturn(rol);
+
+        // Realizar la solicitud PUT y verificar los resultados esperados
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/rol/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(rol)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.nombre").value("Administrador"));
+    }
+
+    @Test
+    public void testEliminarRol() throws Exception {
+        // Mockear el servicio para devolver un rol
+        Long rolId = 1L;
+
+        // Mockear el servicio para devolver 0 empleados asociados al rol
+        when(empleadoService.contarEmpleadosPorRol(eq(rolId))).thenReturn(0);
+
+        // Realizar la solicitud DELETE y verificar los resultados esperados
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/rol/{id}", rolId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Rol borrado: " + rolId));
+
+        // Mockear el servicio para devolver 1 empleado asociado al rol
+        when(empleadoService.contarEmpleadosPorRol(eq(rolId))).thenReturn(1);
+
+        // Realizar la solicitud DELETE y verificar los resultados esperados
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/rol/{id}", rolId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().string("El rol no puede ser eliminado porque esta asignado a un empleado"));
     }
 
 }
