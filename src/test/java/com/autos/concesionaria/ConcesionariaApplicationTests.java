44fb72d055ab0805f025c3380137d53218bc5fbd
LeooZeballos <zeballosleonel3@gmail.com>
2023-06-02 11:50:23 -0300

    removido test de prueba

--- src/test/java/com/autos/concesionaria/ConcesionariaApplicationTests.java
+++ src/test/java/com/autos/concesionaria/ConcesionariaApplicationTests.java
@@ -3,21 +3,11 @@ package com.autos.concesionaria;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
 class ConcesionariaApplicationTests {
 
     @Test
     void contextLoads() {
-    }
-
-    @Test
-    void test() {
-        int a = 3;
-        int b = 4;
-
-        assertEquals(7, a + b);
     }
 
 }
