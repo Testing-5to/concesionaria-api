eb15ad49c2edfa0d71ab658b9c05fc3845ec5414
julianGoia <juliangoia01@gmail.com>
2023-06-09 14:18:22 -0300

    wip: VehiculoControllerTest

---
+++ src/test/java/com/autos/concesionaria/controller/VehiculoControllerTest.java
@@ -0,0 +1,149 @@
+package com.autos.concesionaria.controller;
+
+import com.autos.concesionaria.entity.Modelo;
+import com.autos.concesionaria.entity.Vehiculo;
+import com.autos.concesionaria.entity.Venta;
+import com.autos.concesionaria.service.VehiculoService;
+import com.autos.concesionaria.service.VentaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(VehiculoController.class)
+class VehiculoControllerTest {
+
+    @MockBean
+    private VehiculoService vehiculoService;
+    @MockBean
+    private VentaService ventaService;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getVehiculos() throws Exception {
+        // given
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setAnio(2023);
+        List<Vehiculo> vehiculos = new ArrayList<>();
+        vehiculos.add(vehiculo);
+        // when
+        when(vehiculoService.getVehiculos()).thenReturn(vehiculos);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/vehiculo")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(vehiculos)));
+
+        // when en el request param se envia un string de modelo
+        when(vehiculoService.getVehiculosByModelo(any(String.class))).thenReturn(vehiculos);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/vehiculo")
+                        .content("Audi")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(vehiculos)));
+    }
+
+    @Test
+    void getVehiculoPorId() throws Exception {
+        // given
+        Long id = 1L;
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setId(id);
+        // when
+        when(vehiculoService.getVehiculo(id)).thenReturn(vehiculo);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/vehiculo/{id}",id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(vehiculo)));
+    }
+
+    @Test
+    void guardarVehiculo() throws Exception {
+        // given
+        Long id = 1L;
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setId(id);
+        // when
+        when(vehiculoService.crearVehiculo(any(Vehiculo.class))).thenReturn(vehiculo);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/vehiculo")
+                        .content(mapper.writeValueAsString(vehiculo))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(vehiculo)));
+    }
+
+    @Test
+    void actualizarVehiculo() throws Exception {
+        // given
+        Long id = 1L;
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setId(id);
+        // when
+        when(vehiculoService.actualizarVehiculo(eq(id),any(Vehiculo.class))).thenReturn(vehiculo);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/api/v1/vehiculo/{id}",id)
+                        .content(mapper.writeValueAsString(vehiculo))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(mapper.writeValueAsString(vehiculo)));
+    }
+
+    @Test
+    void borrarVehiculo() throws Exception {
+        // given
+        Long id = 1L;
+        Venta venta = new Venta();
+        List<Venta> ventas = new ArrayList<>();
+        ventas.add(venta);
+
+        // when venta service devuelve 1 vehiculo asociadas a venta
+        when(ventaService.getVentasByVehiculo(eq(id))).thenReturn(ventas);
+
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/vehiculo/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        // when venta service devuelve 0 vehiculos asociadas a venta
+        when(ventaService.getVentasByVehiculo(eq(id))).thenReturn(new ArrayList<>());
+
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/vehiculo/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
